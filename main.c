0c1a7565f02f17c014cc4d06798a1b86cf95ed40
happylittle7 <Lance20501@gmail.com>
2024-05-01 17:26:08 +0000

    Add example for reading character.nekocat

--- main.c
+++ main.c
@@ -13,48 +13,65 @@ int main() {
   FILE *fp;
   char errbuf[200];
 
+
   // 1. Read and parse toml file
-  fp = fopen("sample.toml", "r");
+  fp = fopen("character.nekocat", "r");
   if (!fp) {
-    fatal("cannot open sample.toml - ", strerror(errno));
+    fatal("cannot open character.nekocat - ", strerror(errno));
   }
 
-  toml_table_t *conf = toml_parse_file(fp, errbuf, sizeof(errbuf));
+  //Open character table and save in character
+  toml_table_t *character = toml_parse_file(fp, errbuf, sizeof(errbuf));
   fclose(fp);
 
-  if (!conf) {
+  if (!character) {
     fatal("cannot parse - ", errbuf);
   }
 
-  // 2. Traverse to a table.
-  toml_table_t *server = toml_table_in(conf, "server");
-  if (!server) {
-    fatal("missing [server]", "");
+  // 2. Print how many characters are in the file
+  toml_array_t *charList = toml_array_in(character, "char_list");
+  
+  //charSize: the number of how many characters in the file
+  int charSize = toml_array_nelem(charList);
+
+  printf("Number of characters(charSize): %d\n", charSize);
+
+  // 3. Print all characters idntifiers
+  printf("Character in this file: ");
+  for(int i=0;i<charSize;i++){
+    printf("%s, ",toml_string_at(charList, i).u.s);
   }
+  printf("\n\n");
+
+  // 4. Traverse to a table.
+  for(int i=0;i<charSize;i++){
+    toml_table_t *now = toml_table_in(character, toml_string_at(charList, i).u.s);
+    toml_array_t *nowSpriteList = toml_array_in(now,"sprites_list");
+    int spriteSize=toml_array_nelem(nowSpriteList);
+
+    printf("Character name : %s\n", toml_string_in(now, "name").u.s);
+    printf("Character color : %s\n", toml_string_in(now, "color").u.s);
+    printf("The number of sprites this character have: %d\n",spriteSize);
+    
+    for(int j=0;j<spriteSize;j++){
+      printf("%s: \n",toml_string_at(nowSpriteList,j).u.s);
+      toml_table_t *nowSprite = toml_table_in(now,"sprites");
+      printf("  Filename: %s\n",toml_string_in(nowSprite,toml_string_at(nowSpriteList,j).u.s).u.s);
+      //toml_table_t *nowSprite = toml_table_in(now,toml_string_at(nowSpriteList,j));
+    }
+    
+    //printf("%s\n", toml_string_in(nowSprite,"sad").u.s);
+    printf("\n");
+  }
+  
 
   // 3. Extract values
-  toml_datum_t host = toml_string_in(server, "host");
-  if (!host.ok) {
-    fatal("cannot read server.host", "");
-  }
 
-  toml_array_t *portarray = toml_array_in(server, "port");
-  if (!portarray) {
-    fatal("cannot read server.port", "");
-  }
-
-  printf("host: %s\n", host.u.s);
-  printf("port: ");
-  for (int i = 0;; i++) {
-    toml_datum_t port = toml_int_at(portarray, i);
-    if (!port.ok)
-      break;
-    printf("%d ", (int)port.u.i);
-  }
-  printf("\n");
+  
+  
 
   // 4. Free memory
-  free(host.u.s);
-  toml_free(conf);
+
+  toml_free(character);
   return 0;
 }
