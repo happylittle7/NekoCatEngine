73980cb0d5ba0cc5739fec4265219b3df96d07e3
boyan <cby931001@gmail.com>
2024-06-13 16:51:31 +0800

    Update backpack.c and backpack.h

--- Core/backpack.c
+++ Core/backpack.c
@@ -1,38 +1,85 @@
 #include "backpack.h"
 
-int32_t loadBackpackImages(char* backpackWhiteButtonTexturePath, char* backpackBlackButtonTexturePath, char* whiteBackbackTexturePath, char* blackBackbackTexturePath, SDL_Renderer* renderer)
+void chooseTextColor(uint8_t text_color, SDL_Color *titleTextColor)
 {
-    SDL_Surface* surface = IMG_Load(backpackWhiteButtonTexturePath);
-    if(!surface){
-        printf("Error: %s\n", IMG_GetError());
-        return -1;
+    if (text_color)
+    {
+        titleTextColor->r = 255;
+        titleTextColor->g = 255;
+        titleTextColor->b = 255;
     }
-    backpackWhiteButtonTexture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    surface = IMG_Load(backpackBlackButtonTexturePath);
-    if(!surface){
-        printf("Error: %s\n", IMG_GetError());
-        return -1;
+    else
+    {
+        titleTextColor->r = 0;
+        titleTextColor->g = 0;
+        titleTextColor->b = 0;
     }
-    backpackBlackButtonTexture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+    return;
+}
 
-    surface = IMG_Load(whiteBackbackTexturePath);
-    if(!surface){
-        printf("Error: %s\n", IMG_GetError());
-        return -1;
+void displayTextWithShadow(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Color titleTextColor, int32_t x, int32_t y, int32_t move, char *option_x, char *option_y)
+{
+    // option_x = "LEFT, "CENTER" or "RIGHT"
+    // option_y = "TOP, "CENTER" or "BOTTOM"
+
+    SDL_Color shadowColor = {0, 0, 0};
+    displayText(renderer, font, text, shadowColor, x + move, y + move, option_x, option_y);
+    displayText(renderer, font, text, titleTextColor, x, y, option_x, option_y);
+}
+
+void displayText(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Color titleTextColor, int32_t x, int32_t y, char *option_x, char *option_y)
+{
+    // option_x = "LEFT, "CENTER" or "RIGHT"
+    // option_y = "TOP, "CENTER" or "BOTTOM"
+
+    SDL_Surface *textSurface = TTF_RenderUTF8_Solid(font, text, titleTextColor);
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    int text_width = textSurface->w;  // 文本寬度
+    int text_height = textSurface->h; // 文本高度
+
+    if (option_x == "CENTER")
+    {
+        x = x - text_width / 2;
     }
-    whiteBackbackTexture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    surface = IMG_Load(blackBackbackTexturePath);
-    if(!surface){
-        printf("Error: %s\n", IMG_GetError());
-        return -1;
+    else if (option_x == "RIGHT")
+    {
+        x = x - text_width;
     }
-    blackBackbackTexture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
 
-    return 0;
+    if (option_y == "CENTER")
+    {
+        y = y - text_height / 2;
+    }
+    else if (option_y == "BOTTOM")
+    {
+        y = y - text_height;
+    }
+
+    SDL_Rect renderQuad = {x, y, text_width, text_height};    // 定義渲染區域
+    SDL_RenderCopy(renderer, textTexture, NULL, &renderQuad); // 執行渲染操作
+
+    SDL_FreeSurface(textSurface);    // 釋放表面資源
+    SDL_DestroyTexture(textTexture); // 銷毀紋理資源
+
+    return;
+}
+
+void backpackInit(backpackData *backpack_data)
+{
+    // 初始化背包
+    backpack_data->backpack_item_count = 0;
+    backpack_data->backpack_normal_block_width = 0.15;
+    backpack_data->backpack_normal_block_height = 0.15;
+    backpack_data->backpack_normal_block_gap = 0.05;
+    backpack_data->backpack_01_x = 0.1;
+    backpack_data->backpack_01_y = 0.1;
+    backpack_data->backpack_02_x = 0.1 + 0.15 + 0.05;
+    backpack_data->backpack_02_y = 0.1;
+    backpack_data->backpack_03_x = 0.1 + 0.15 + 0.05 + 0.15 + 0.05;
+    backpack_data->backpack_03_y = 0.1;
+    backpack_data->backpack_04_x = 0.1;
+    backpack_data->backpack_04_y = 0.1 + 0.15 + 0.05;
+    backpack_data->backpack_05_x = 0.1 + 0.15 + 0.05;
+    backpack_data->backpack_05_y = 0.1 + 0.15 + 0.05;
+    backpack_data->backpack_06_x = 0.1 + 0.15 + 0.05 + 0.15 + 0.05;
 }
--- Core/backpack.h
+++ Core/backpack.h
@@ -6,9 +6,48 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 
-static SDL_Texture* backpackWhiteButtonTexture;
-static SDL_Texture* backpackBlackButtonTexture;
-static SDL_Texture* whiteBackbackTexture;
-static SDL_Texture* blackBackbackTexture;
+#pragma once
 
-int32_t loadBackpackImages(char* backpackWhiteButtonTexturePath, char* backpackBlackButtonTexturePath, char* whiteBackbackTexturePath, char* blackBackbackTexturePath, SDL_Renderer* renderer);
+typedef struct _backpackItem
+{
+    uint8_t status; // 0: nothing, 1: obtained, 2: used
+    char name[512];
+    char description[1024];
+    char image_path[1024];
+} backpackItem;
+
+typedef struct _backpackData
+{
+    double backpack_item_count;
+    double backpack_normal_block_width;
+    double backpack_normal_block_height;
+    double backpack_normal_block_gap;
+    double backpack_01_x;
+    double backpack_01_y;
+    double backpack_02_x;
+    double backpack_02_y;
+    double backpack_03_x;
+    double backpack_03_y;
+    double backpack_04_x;
+    double backpack_04_y;
+    double backpack_05_x;
+    double backpack_05_y;
+    double backpack_06_x;
+    double backpack_06_y;
+} backpackData;
+
+typedef struct _backpackPath
+{
+    // item 的圖片路徑必須在 backpackItem 中設定, 並非在此設定
+    char background_path[1024];
+    char font_path[1024];
+    char black_block_path[1024];
+} backpackPath;
+
+void chooseTextColor(uint8_t text_color, SDL_Color* titleTextColor);
+
+void displayText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color titleTextColor, int32_t x, int32_t y, char* option_x, char* option_y);
+
+void displayTextWithShadow(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color titleTextColor, int32_t x, int32_t y, int32_t move, char* option_x, char* option_y);
+
+void backpackInit(backpackData* backpack_data);
